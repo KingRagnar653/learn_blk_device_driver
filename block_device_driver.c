0dba5b24ce5fb3d464ad95c4e1c8b1a50f812349
KingRagnar653 <rahulbalurs@gmail.com>
2024-10-12 23:18:45 +0530

    Added alloc add disk functionality to generated gendisk struct

--- block_device_driver.c
+++ block_device_driver.c
@@ -1,6 +1,9 @@
 /*
     Linux kernel provide a whole different subsystem called
         block I/O (block layer) subsystem
+    register_blkdev Not allocated disk to the system
+        genhd.h -> allocate and add and delete disk(device) to the system
+        * add_disk (even during the call), the methods will be active (so called after initialisation)
 */
 #include<linux/fs.h>
 #include<linux/init.h>
@@ -8,7 +11,24 @@
 #include<linux/kernel.h> 
 
 #define BLK_NAME "blk_rs_device"
+#define BLOCK_MINOR 1
 int major_num;
+
+/*
+ * to store all elements describing block device
+ */
+static struct my_blk_dev {
+    struct gendisk *gd;
+} dev;
+
+void create_disk(struct my_blk_dev *dev) {
+    dev->gd = alloc_disk(BLOCK_MINOR);
+    add_disk(dev->gd);
+}
+
+void delete_disk(struct my_blk_dev *dev) {
+    del_gendisk(dev->gd);
+}
 
 
 static int __init blk_init(void) {
@@ -21,11 +41,14 @@ static int __init blk_init(void) {
         return -EBUSY;
     }
     printk("INIT: major_num %d\n", major_num);
+    
+    create_disk(&dev);
     return 0;
 }
 
 static void __exit blk_exit(void) {
     if (major_num) {
+        delete_blk_device(&dev);
         unregister_blkdev(major_num, BLK_NAME);
     }
 }
