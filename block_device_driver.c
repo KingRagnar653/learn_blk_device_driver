5b28cbae7f9622bf904686e3787a2fbff1cae08b
KingRagnar653 <rahulbalurs@gmail.com>
2024-10-13 10:28:00 +0530

    Added queue initialisation

--- block_device_driver.c
+++ block_device_driver.c
@@ -6,6 +6,13 @@
         * add_disk (even during the call), the methods will be active (so called after initialisation)
     blk_queue_logical_block_size() to inform kernel of different sector size , although communication btw device driver still be 512 sector size
 */
+/*
+    Multi queue block layer
+    ------------------------
+    * software stagin queue for each core or cpu node
+        -> used by IO subsystem and scheduler
+    * hardware dispatch queue (struct blk_mq_hw_ctx)
+*/
 #include<linux/fs.h>
 #include<linux/init.h>
 #include<linux/module.h>
@@ -16,6 +23,7 @@
 #define BLK_NAME "blk_rs_device"
 #define BLOCK_MINOR 1
 #define NR_SECTORS 1024
+#define KERNEL_SECTOR 512
 
 int major_num;
 
@@ -25,8 +33,18 @@ int major_num;
 static struct my_blk_dev {
     spinlock_t lock;
     struct gendisk *gd;
+    struct blk_mq_tag_set tag_set;
     struct request_queue *queue;
 } dev;
+
+/*
+ * functions and struct to intialise queue structure
+ */
+static blk_status_t my_block_request(struct blk_mq_hw_ctx *hctx, const struct blk_mq_queue_data *bd);
+
+struct blk_mq_ops my_queue_ops = {
+    .queue_rq = my_block_request,
+};
 
 int my_block_open(struct block_device *bdev, fmode_t mode) {
     printk("OPEN: blk device open\n");
@@ -45,6 +63,7 @@ struct block_device_operations my_block_ops = {
 };
 
 void create_disk(struct my_blk_dev *dev) {
+    int err;
     dev->gd = alloc_disk(BLOCK_MINOR);
     /*
      * Fill the important fields for gendisk
@@ -57,7 +76,35 @@ void create_disk(struct my_blk_dev *dev) {
     snprintf(dev->gd->name, 32, "rsblock");
     set_capacity(dev->gd, NR_SECTORS);
 
+    /* initialise tag set */
+    dev->tag_set.ops = &my_queue_ops;
+    dev->tag_set.nr_hw_queues = 1;
+    dev->tag_set.queue_depth = 128;
+    dev->tag_set.cmd_size = 0;
+    dev->tag_set.flags = BLK_MQ_F_SHOULD_MERGE;
+    dev->tag_set.numa_node = NO_NUMA_NODE;
+
+    err = blk_mq_alloc_tag_set(&dev->tag_set);
+    if (err) {
+        goto out;
+    }
+
+    /*
+     * Allocate queue
+     */
+    dev->queue = blk_mq_init_queue(&dev->tag_set);
+    if (IS_ERR(dev->queue))
+        goto out_blk_init;
+    
+    blk_queue_logical_block_size(dev->queue, KERNEL_SECTOR);
+
+    dev->queue->queuedata = dev;
+
     add_disk(dev->gd);
+out_blk_init:
+    blk_mq_free_tag_set(&dev->tag_set);
+out:
+    return -ENOMEM;
 }
 
 void delete_disk(struct my_blk_dev *dev) {
@@ -83,6 +130,7 @@ static int __init blk_init(void) {
 static void __exit blk_exit(void) {
     if (major_num) {
         delete_blk_device(&dev);
+        blk_mq_free_tag_set(&dev->tag_set);
         unregister_blkdev(major_num, BLK_NAME);
     }
 }
